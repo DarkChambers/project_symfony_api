31658bd0a03799b8940e8a623b4402973ba6cb21
Christophe <monteilchristophe@live.fr>
2019-09-16 14:44:16 +0200

    création entity blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 //use annotations to define route and parameters
@@ -30,15 +32,18 @@ class BlogController extends AbstractController
         ],
     ];
     //define a default value for parameters, use php bin\console debug:router to see routes
-
+    //handle parameters from url by Request
+    //symfony must return a Response like Object
     /**
      * @Route("/{page}", name="blog_list", defaults={"page":5})
      */
-    public function list($page = 1)
+    public function list($page = 1, Request $request)
     {
-        return new JsonResponse(
+        $limit = $request->get('limit', 10);
+        return $this->json(
             [
                 'page' => $page,
+                'limit' => $limit,
                 'data' => array_map(
                     function ($item) {
                         return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
@@ -55,7 +60,7 @@ class BlogController extends AbstractController
      */
     public function post($id)
     {
-        return new JsonResponse(
+        return $this->json(
             self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
         );
     }
@@ -64,8 +69,18 @@ class BlogController extends AbstractController
      */
     public function postBySlug($slug)
     {
-        return new JsonResponse(
+        return $this->json(
             self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
         );
+    }
+
+    /**
+     * @Route('/add', name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @Var Serializer $serializer */
+        $serializer = $this->get('serializer');
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
     }
 }
