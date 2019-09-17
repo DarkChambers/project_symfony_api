cfcb85eab8bafb574dbb75d443c3c740169be76b
Christophe <monteilchristophe@live.fr>
2019-09-17 10:58:14 +0200

    add deleting function

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,7 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 //use annotations to define route and parameters
 /**
@@ -22,6 +24,7 @@ class BlogController extends AbstractController
     //symfony must return a Response like Object
     //Do not forger to add a requirement to distinct the route from the add route which received a parameters too
     //add property_info : true to config
+
     /**
      * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"})
      */
@@ -29,7 +32,7 @@ class BlogController extends AbstractController
     {
 
         $limit = $request->get('limit', 10);
-        $repository=$this->getDoctrine()->getRepository(BlogPost::class);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
         $items = $repository->findAll();
         return $this->json(
             [
@@ -39,29 +42,38 @@ class BlogController extends AbstractController
                     function (BlogPost $item) {
                         return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
                     },
-                    $items)
+                    $items
+                )
             ]
         );
     }
 
     //make different route by requirements
     /**
-     * @Route("/post/{id}",name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}",name="blog_by_id", requirements={"id"="\d+"},methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost")
      */
-    public function post($id)
+    public function post($post)
     {
-        return $this->json(
-            $this->getDoctrine()->getRepository(BlogPost::class)->find($id)
-        );
+        //     return $this->json(
+        //         $this->getDoctrine()->getRepository(BlogPost::class)->find($id)
+        //     );
+        return $this->json($post);
     }
+
     /**
-     * @Route("/post/{slug}",name="blog_by_slug")
+     * @Route("/post/{slug}",name="blog_by_slug",methods={"GET"})
+     * The beow annotation is not required when $post is typehinted with BlogSpot 
+     * and route parameter name matches any field on the BlogSpost entity
+     * @ParamConverter("post",class="App:BlogPost", options={"mapping":{"slug":"slug"}})
      */
-    public function postBySlug($slug)
+    public function postBySlug(BlogPost $post)
     {
-        return $this->json(
-            $this->getDoctrine()->getRepository(BlogPost::class)->findBy(['slug'=>$slug])
-        );
+
+        return $this->json($post);
+        // return $this->json(
+        //     $this->getDoctrine()->getRepository(BlogPost::class)->findOneBy(['slug'=>$slug])
+        // );
     }
 
     /**
@@ -79,4 +91,15 @@ class BlogController extends AbstractController
         $em->flush();
         return $this->json($blogPost);
     }
+
+/**
+ * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+ */
+    public function delete(BlogPost $post){
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+
+        return new JsonResponse(null,Response::HTTP_NO_CONTENT);
+        }
 }
